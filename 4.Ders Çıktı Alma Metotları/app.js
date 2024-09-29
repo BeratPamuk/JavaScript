26551c51097ff3b05781c5c039ca0ee3dce04fc7
Berat Pamuk <beratpamuk@Berat-MacBook-Air.local>
2024-09-29 20:23:47 +0300

    4.ders bitti

---
+++ 4.Ders Çıktı Alma Metotları/app.js
@@ -0,0 +1,8 @@
+//1.Yöntem (Sayfa çıktısı)
+document.writeln("Welcome, <br> Dear Berat Pamuk"); //sayfaya yazdırır
+
+//2.Yöntem (Console çıktısı)
+console.log("Hello World"); //console yazdırır
+
+//3.Yöntem (Uyarı çıktısı)
+alert("Hello World");
